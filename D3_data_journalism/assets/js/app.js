7b31ff10f1f55e614d86c64e9c6cf4bc1a083b1c
tao10 <63757639+topshelf10@users.noreply.github.com>
2020-12-09 22:18:06 -0600

    updated html and js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,99 +1,108 @@
 var svgWidth = 960;
 var svgHeight = 500;
 
-var margin = {
+var chartMargin = {
   top: 20,
   right: 40,
   bottom: 80,
   left: 100
 };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select("#svg-area")
+  .select("#scatter")
+  //.select("body .container .row div div")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+
+
 // Append an SVG group
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-// Initial Params
-var chosenXAxis = "poverty";
+d3.csv("/D3_data_journalism/assets/data/data.csv").then(function (data){
+    console.log("hi");
+    var chosenXAxis = "poverty";
 
-// Parse Data 
-data.forEach(function (data) {
-  data.healthcare = +data.healthcare;
-  data.poverty = +data.poverty;
-});
+    data.forEach(function (data) {
+      data.healthcare = +data.healthcare;
+      data.poverty = +data.poverty;
+    });
+    var xLinearScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.poverty-1))
+    .range([0, chartWidth]);
 
-// Create scale functions
+    var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.healthcare)])
+    .range([chartHeight, 0]);
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 
-var yLinearScale = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.healthcare)])
-  .range([chartHeight, 0])
 
-// Create axis functions
-var bottomAxis = d3.axisBottom(xLinearScale);
-var leftAxis = d3.axisLeft(yLinearScale);
+    // Append axes to the chart
+    chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
 
-// Append axes to the chart
-chartGroup.append("g")
-  .classed("axis", true)
-  .attr("transform", `translate(0, ${chartHeight})`)
-  .call(bottomAxis);
+    chartGroup.append("g")
+    .classed("axis", true)
+    .call(leftAxis);
 
-chartGroup.append("g")
-  .classed("axis", true)
-  .call(leftAxis);
+    // Create circles
+    var circlesGroup = chartGroup.selectAll("Circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "#8ABCFF")
+    .attr("opacity", "0.5");
 
-// Create circles
-var circlesGroup = chartGroup.selectAll("Circle")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.poverty))
-  .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", "15")
-  .attr("fill", "blue")
-  .attr("opacity", "0.5");
+    var circleLabels = chartGroup.selectAll(null).data(data).enter().append("text");
 
-var circleLabels = chartGroup.selectAll(null).data(data).enter().append("text");
+    circleLabels
+    .attr("x", function (d) {
+      return xLinearScale(d.poverty);
+    })
+    .attr("y", function (d) {
+      return yLinearScale(d.healthcare);
+    })
+    .text(function (d) {
+      return d.abbr;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "10px")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white");
 
-// Create circle labels
-circleLabels
-  .attr("x", function (d) {
-    return xLinearScale(d.poverty);
-  })
-  .attr("y", function (d) {
-    return yLinearScale(d.healthcare);
-  })
-  .text(function (d) {
-    return d.abbr;
-  })
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "10px")
-  .attr("text-anchor", "middle")
-  .attr("fill", "white");
+    // Create axis labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - chartMargin.left + 40)
+    .attr("x", 0 - (chartHeight / 2))
+    //.attr("dy", "lem")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
 
-// Create axis labels
-chartGroup.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left + 40)
-  .attr("x", 0 - (height / 2))
-  .attr("dy", "1em")
-  .attr("class", "axisText")
-  .text("Lacks Healthcare (%)");
+    chartGroup.append("text")
+    .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
 
-chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-  .attr("class", "axisText")
-  .text("In Poverty (%)");
 
-});
-  
+
+
+    // chartGroup.catch(function (error) {
+    //   console.log(error);
+    // });
+
+})
